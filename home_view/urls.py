5b7b48840672dbc644d2f3b28484b681ab89664b
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-09-30 22:55:31 +0200

    Views setup

--- home_view/urls.py
+++ home_view/urls.py
@@ -3,5 +3,8 @@ from . views import *
 
 
 urlpatterns = [
+    path('home/', home, name='home'),
+    path('about/', about, name='about'),
+    path('book/', book, name='book'),
     path('menu/', menu, name='menu'),
 ]
